8bff331852da8583b417d3630c59f64531c83b01
UnwantedFriedChicken <unwantedfriedchicken@gmail.com>
2021-02-09 05:29:49 +0700

    fix dist version

--- dist/UFCGuideAction.js
+++ dist/UFCGuideAction.js
@@ -1,4 +1,49 @@
-/* eslint-disable no-redeclare */
+/*:
+@target MZ
+
+@plugindesc Add guide action
+@author Unwanted Fried Chicken
+@url https://unwantedfriedchicken.itch.io
+@help
+Author: UnwantedFriedChicken
+Version: 1.1
+Itch.io : https://unwantedfriedchicken.itch.io
+
+Add guide action 
+
+@param blockColor
+@text Block Color
+@type text
+@desc Set the color of blocked action. Default = #e63946
+@default #e63946
+
+@param blockColorAlpha
+@text Block Color Alpha
+@type number
+@decimals 1
+@min 0
+@max 1
+@desc Alpha color of blocked action
+@default 1.0
+
+@param openColor
+@text Open Color
+@type text
+@desc Set the color of opened action. Default = #5BFF54
+@default #5BFF54
+
+@param openColorAlpha
+@text Open Color Alpha
+@type number
+@decimals 1
+@min 0
+@max 1
+@desc Alpha color of opened action
+@default 1.0
+
+*/
+
+
 var Imported = Imported || {};
 Imported.UFCGuideAction = true;
 
@@ -297,8 +342,6 @@ GuideAction.prototype.checkGrid = function (x, y) {
 
   let terrainTag = $gameMap.terrainTag(x, y);
   let regionID = $gameMap.regionId(x, y);
-
-  //allows for Place everywhere, except exceptTerrain or exceptRegion
   if (
     this.getOnlyTerrain.length <= 0 &&
     this.getOnlyRegion.length <= 0 &&
@@ -463,7 +506,6 @@ GuideAction.prototype.isCollidedWithVehicles = function (x, y) {
 };
 
 GuideAction.prototype.getPosition = function () {
-  // Update to current gameplayer incase the position not updated
   this._x = $gamePlayer._x;
   this._y = $gamePlayer._y;
   this._d = $gamePlayer._direction;
--- dist/UFCTextHelper.js
+++ dist/UFCTextHelper.js
@@ -1,4 +1,35 @@
-/* eslint-disable no-redeclare */
+/*:
+@target MZ
+
+@plugindesc Add Text Helper
+@author Unwanted Fried Chicken
+@url https://unwantedfriedchicken.itch.io
+@help
+Author: UnwantedFriedChicken
+Version: 1.0
+Itch.io : https://unwantedfriedchicken.itch.io
+
+This is helper text
+
+@param default
+@text Default Text Settings
+
+@param defaultColor
+@parent default
+@type text
+@text Text Color
+@desc Default Text Color
+@default #ffffff
+
+@param defaultSize
+@parent default
+@type number
+@text Size
+@desc Default Text Size
+@default 28
+*/
+
+
 var Imported = Imported || {};
 Imported.UFCTextHelper = true;
 
